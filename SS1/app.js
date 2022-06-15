4b3a2f44504c86c7a4cd55ecb229dfa47cfc8ff7
hieu1966hn <hieu1966hn@gmail.com>
2022-06-15 19:48:51 +0700

    Done SS1, Create setup SS2

--- SS1/app.js
+++ SS1/app.js
@@ -1,39 +1,46 @@
-const form = document.getElementById("form")
-const input = document.getElementById("input")
-const button = document.getElementById("button")
-const todo = document.getElementById("todo")
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const todo = document.getElementById("todo");
 
-let todoList = []
+let todoList = [];
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  addTodo();
+  event.preventDefault();// dùng dể ngăn sự kiện reload mặc định của trình duyệt
+  addTodo(); // Hàm thêm nội dung sẽ làm
 })
 
 const addTodo = () => {
-  const newTodo = input.value
+  const newTodo = input.value;
   if (!newTodo) return console.log("No value! ❌") || alert("No value! ❌")
+
   todoList.push({
     text: newTodo,
     completed: false
   })
   localStorage.setItem("todos", JSON.stringify(todoList))
-  render()
+  render();
 }
 
 const render = () => {
-  todo.innerHTML = null
-  todoList = JSON.parse(localStorage.getItem("todos")) || []
+  todo.innerHTML = null // null: là giá trị 
+  todoList = JSON.parse(localStorage.getItem("todos")) || []; // Lấy dữ liệu được lưu trên localStorage dưới dạng mảng
   console.log(todoList)
+
+  // duyệt mảng
   for (let i = 0; i < todoList.length; i++) {
-    let li = document.createElement("li")
+
+    // 
+    let li = document.createElement("li");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     todo.appendChild(li)
     li.innerHTML = `${todoList[i].text}`
     li.appendChild(checkbox)
+
+
     checkbox.addEventListener("click", (event) => {
-      todoList[i].completed = event.target.checked
+      todoList[i].completed = event.target.checked // trả về kết quả là true
 
       if (todoList[i].completed) {
         li.classList.add("completed");
@@ -45,6 +52,8 @@ const render = () => {
         checkbox.checked = todoList[i].completed;
       }
     })
+
+
   }
 
 }
