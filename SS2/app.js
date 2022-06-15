064a0c25935e5140acde736d1b1277d0b5f55bb8
hieu1966hn <hieu1966hn@gmail.com>
2022-06-15 20:08:45 +0700

    done vd về thuộc tính Object Assign

--- SS2/app.js
+++ SS2/app.js
@@ -1,18 +1,49 @@
-let obj1 = {
-  name: "John",
-  age: 24,
-  address: "22 Jump Street"
+// let obj1 = {
+//   name: "John",
+//   age: 24,
+//   address: "22 Jump Street"
+// }
+
+// let obj2 = {
+//   name: "Hiếu",
+//   age: 25,
+//   hobbies: "Sleeping, coding"
+// }
+
+
+
+// /// Đề bài: Sao chép toàn bộ thuộc tính và giá trị của đối tượng 1 và 2 vào 1 biến mới có tên là obj3
+// let obj3 = Object.assign(obj1, obj2);
+// console.log("obj3: ", obj3);
+
+
+//=>> Object có tất cả các thuộc tính từ các đối tượng khác
+
+
+// VD mới về Object.assign
+
+let VT = {
+  name: "Nguyễn Vũ Tiến",
+  age: 14,
+  hobbies: "Playing game, sleeping",
+  TienID: "001"
 }
 
-let obj2 = {
-  name: "Hiếu",
-  age: 25,
-  hobbies: "Sleeping, coding"
+let TH = {
+  name: "Nguyễn Trung Hiếu",
+  age: 17,
+  hobbies: "Nội trợ",
+  HieuID: "002"
+}
+
+let GN = {
+  name: "Lương Gia Nam",
+  age: 17,
+  hobbies: "Playing game, cooking",
+  NamID: "003"
 }
 
 
-
-/// Đề bài: Sao chép toàn bộ thuộc tính và giá trị của đối tượng 1 và 2 vào 1 biến mới có tên là obj3
-let obj3 = Object.assign(obj1, obj2);
-console.log("obj3: ", obj3);
+let NPS_JSI05 = Object.assign(VT, TH, GN);
+console.log("NPS_JSI05: ", NPS_JSI05);
 
